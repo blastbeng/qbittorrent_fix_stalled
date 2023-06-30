a0f64c6f3c8590fcd787581ae50ac09da9331342
blastbeng <rodfval.fv@gmail.com>
2023-06-30 14:00:17 +0200

    Update main.py

--- qbittorrentFixStalled/main.py
+++ qbittorrentFixStalled/main.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import time
+import logging
 import qbittorrentapi
 import datetime as dt
 
@@ -8,28 +10,38 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HOST=os.environ.get("HOST"),
-PORT=int(os.environ.get("PORT")),
-USER=os.environ.get("USER"),
-PASS=os.environ.get("PASS"),
-
 schedule = Scheduler()
 
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=int(os.environ.get("LOG_LEVEL")),
+        datefmt='%Y-%m-%d %H:%M:%S')
+
 def fix_stalled():
-    conn_info = dict(
-        host=HOST,
-        port=PORT,
-        username=USER,
-        password=PASS,
-    )
-    qbt_client = qbittorrentapi.Client(**conn_info)
+    try:
+        conn_info = dict(
+            host=os.environ.get("HOST"),
+            port=os.environ.get("PORT"),
+            username=os.environ.get("USER"),
+            password=os.environ.get("PASS"),
+        )
+        logging.info("Connecting...")
+        qbt_client = qbittorrentapi.Client(**conn_info)
 
-    stalled = qbt_client.torrents.info(status_filter="stalled_downloading")
+        logging.info("Getting stalled torrents")
+        stalled = qbt_client.torrents.info(status_filter="stalled_downloading")
 
-    for torrent in stalled.data:
-        if torrent.info.num_seeds == 0 and torrent.info.time_active > 1800:
-            qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
-        
+        for torrent in stalled.data:
+            if torrent.info.num_seeds == 0 and torrent.info.time_active > 1800:
+                qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
+                logging.info("Setting bottom priority on %s", torrent.name)
+
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        logging.error("%s %s %s", exc_type, fname, exc_tb.tb_lineno, exc_info=1)
+          
+fix_stalled()
 
 schedule.cyclic(dt.timedelta(minutes=30), fix_stalled)
 
