c3ee91b44d9409feea4cf3f9cc3d9817ed2d3a10
blastbeng <rodfval.fv@gmail.com>
2023-06-30 13:05:42 +0200

    Update main.py

--- qbittorrentFixStalled/main.py
+++ qbittorrentFixStalled/main.py
@@ -8,29 +8,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+HOST=os.environ.get("HOST"),
+PORT=int(os.environ.get("PORT")),
+USER=os.environ.get("USER"),
+PASS=os.environ.get("PASS"),
+
 schedule = Scheduler()
 
 def fix_stalled():
     conn_info = dict(
-        host=os.environ.get("HOST"),
-        port=int(os.environ.get("PORT")),
-        username=os.environ.get("USER"),
-        password=os.environ.get("PASS"),
+        host=HOST,
+        port=PORT,
+        username=USER,
+        password=PASS,
     )
     qbt_client = qbittorrentapi.Client(**conn_info)
 
     stalled = qbt_client.torrents.info(status_filter="stalled_downloading")
 
-    hashdropqueue = []
-
     for torrent in stalled.data:
-        if torrent.info.num_seeds == 0 and torrent.info.time_active > 3600:
-            hashdropqueue.append(torrent.hash)
+        if torrent.info.num_seeds == 0 and torrent.info.time_active > 1800:
+            qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
+        
 
-    if len(hashdropqueue) > 0:
-        qbt_client.torrents.decrease_priority(torrent_hashes=hashdropqueue)
-
-schedule.cyclic(dt.timedelta(minutes=90), fix_stalled)
+schedule.cyclic(dt.timedelta(minutes=30), fix_stalled)
 
 while True:
     schedule.exec_jobs()
