4d7491ffbaabe36e76de63c78d84fe2f8c0c88df
blastbeng <rodfval.fv@gmail.com>
2023-07-03 19:22:48 +0200

    Update main.py

--- qbittorrentFixStalled/main.py
+++ qbittorrentFixStalled/main.py
@@ -19,28 +19,48 @@ logging.basicConfig(
 
 def fix_stalled():
     try:
+        logging.info("--- START fix_stalled START ---")
         conn_info = dict(
             host=os.environ.get("HOST"),
-            port=os.environ.get("PORT"),
+            port=int(os.environ.get("PORT")),
             username=os.environ.get("USER"),
             password=os.environ.get("PASS"),
         )
         logging.info("Connecting...")
         qbt_client = qbittorrentapi.Client(**conn_info)
 
-        logging.info("Getting stalled torrents")
-        stalled = qbt_client.torrents.info(status_filter="stalled_downloading")
-
-        for torrent in stalled.data:
-            if torrent.info.num_seeds == 0 and torrent.info.time_active > 1800:
-                qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
-                logging.info("Setting bottom priority on %s", torrent.name)
+        decrease_prio(qbt_client, qbt_client.torrents.info(status_filter="stalled_downloading"))
+        decrease_prio(qbt_client, qbt_client.torrents.info(status_filter="active"))
 
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         logging.error("%s %s %s", exc_type, fname, exc_tb.tb_lineno, exc_info=1)
+    finally:
+        logging.info("--- END fix_stalled END ---")
           
+
+def decrease_prio(qbt_client, data):
+    for torrent in data:
+        logging.info("Torrent: %s", torrent.info.hash)
+        logging.info("      - hash:        %s", torrent.info.hash)
+        logging.info("      - state:       %s", torrent.info.state)
+        logging.info("      - num_seeds:   %s", torrent.info.num_seeds)
+        logging.info("      - time_active: %s", str(torrent.info.time_active))
+        if torrent.state == 'stalledDL' and torrent.info.time_active > 1:
+            logging.info("      - action: %s", "setting bottom priority")
+            qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
+        elif torrent.state == 'metaDL' and torrent.info.num_seeds == 0 and torrent.info.time_active > 1:
+            logging.info("      - action: %s", "setting bottom priority")
+            qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
+        elif torrent.state == 'downloading' and torrent.info.num_seeds == 0 and torrent.info.time_active > 1:
+            logging.info("      - action: %s", "setting bottom priority")
+            qbt_client.torrents.bottom_priority(torrent_hashes=torrent.hash)
+        else:
+            logging.info("      - action: %s", "skipped")
+
+
+
 fix_stalled()
 
 schedule.cyclic(dt.timedelta(minutes=30), fix_stalled)
